2616a88deaf24d92dd9405ed0fea2e0763547fa7
Alexander Orlov <alexander.orlov@loxal.net>
2012-09-20 18:15:28 +0200

    Warning suppressed builds again

--- src/main/java/loxal/epvin/core/client/ui/EmployeeEditorWorkflow.java
+++ src/main/java/loxal/epvin/core/client/ui/EmployeeEditorWorkflow.java
@@ -16,7 +16,7 @@ import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
 import com.google.web.bindery.requestfactory.gwt.client.RequestFactoryEditorDriver;
-import com.google.web.bindery.requestfactory.shared.EntityProxy;
+import com.google.web.bindery.requestfactory.shared.EntityProxyId;
 import com.google.web.bindery.requestfactory.shared.Receiver;
 import com.google.web.bindery.requestfactory.shared.Request;
 import com.google.web.bindery.requestfactory.shared.RequestContext;
@@ -33,189 +33,190 @@ import javax.validation.ConstraintViolation;
 import java.util.Set;
 
 public class EmployeeEditorWorkflow {
-    interface Binder extends UiBinder<DialogBox, EmployeeEditorWorkflow> {
-        static final Binder BINDER = GWT.create(Binder.class);
+  interface Binder extends UiBinder<DialogBox, EmployeeEditorWorkflow> {
+    static final Binder BINDER = GWT.create(Binder.class);
+  }
+
+  interface Driver extends RequestFactoryEditorDriver<EmployeeProxy, EmployeeEditor> {
+    static final Driver DRIVER = GWT.create(Driver.class);
+  }
+
+  @UiField
+  EmployeeEditor employeeEditor;
+  @UiField
+  DialogBox dialog;
+  @UiField
+  Button ok;
+  @UiField
+  Button cancel;
+  @UiField(provided = true)
+  ClientFactory cf;
+
+  private EmployeeProxy employee;
+
+  public void create() { // TODO move this to EmployeeActivity and make usage of EmployeeActivities Cf?
+    final EmployeeReqCtx reqCtx = cf.getRf().employeeReqCtx();
+    employee = reqCtx.create(EmployeeProxy.class);
+    edit(reqCtx);
+  }
+
+  private void save() {
+    new StatusBar(
+        cf,
+        SafeHtmlUtils.fromSafeConstant(I18nConstants.INSTANCE.updating()),
+        StatusBar.Kind.INFO,
+        null);
+
+    final RequestContext driverCtx = Driver.DRIVER.flush();
+    if (Driver.DRIVER.hasErrors()) {
+      dialog.setText("Invalid input");
+
+      new StatusBar(
+          cf,
+          SafeHtmlUtils.fromSafeConstant(I18nConstants.INSTANCE.editorHasErrors()),
+          StatusBar.Kind.APP_ERROR,
+          null);
+
+      return;
     }
 
-    interface Driver extends RequestFactoryEditorDriver<EmployeeProxy, EmployeeEditor> {
-        static final Driver DRIVER = GWT.create(Driver.class);
-    }
-
-    @UiField
-    EmployeeEditor employeeEditor;
-    @UiField
-    DialogBox dialog;
-    @UiField
-    Button ok;
-    @UiField
-    Button cancel;
-    @UiField(provided = true)
-    ClientFactory cf;
-
-    private EmployeeProxy employee;
-
-    public void create() { // TODO move this to EmployeeActivity and make usage of EmployeeActivities Cf?
-        final EmployeeReqCtx reqCtx = cf.getRf().employeeReqCtx();
-        employee = reqCtx.create(EmployeeProxy.class);
-        edit(reqCtx);
-    }
-
-    private void save() {
-        new StatusBar(
-                cf,
-                SafeHtmlUtils.fromSafeConstant(I18nConstants.INSTANCE.updating()),
-                StatusBar.Kind.INFO,
-                null);
-
-        final RequestContext driverCtx = Driver.DRIVER.flush();
-        if (Driver.DRIVER.hasErrors()) {
-            dialog.setText("Invalid input");
-
-            new StatusBar(
-                    cf,
-                    SafeHtmlUtils.fromSafeConstant(I18nConstants.INSTANCE.editorHasErrors()),
-                    StatusBar.Kind.APP_ERROR,
-                    null);
-
-            return;
-        }
-
-        driverCtx.fire(new Receiver<Void>() {
-            @Override
-            public void onConstraintViolation(Set<ConstraintViolation<?>> violations) {
-                dialog.setText("Errors detected");
-                Driver.DRIVER.setConstraintViolations(violations);
+    driverCtx.fire(new Receiver<Void>() {
+      @Override
+      public void onConstraintViolation(Set<ConstraintViolation<?>> violations) {
+        dialog.setText("Errors detected");
+        Driver.DRIVER.setConstraintViolations(violations);
 //        List<EditorError> editorErrors = Driver.DRIVER.getErrors();
 //        for (EditorError editorError : editorErrors) {
 //          Logger.getLogger("editorError.getMessage(): ").log(Level.INFO, editorError.getMessage() + "");
 //        }
 
-                cf.getEb().fireEvent(new DoneEvent());
+        cf.getEb().fireEvent(new DoneEvent());
 
-                final StringBuilder sb = new StringBuilder();
-                sb.append("<ol>");
-                for (final ConstraintViolation<?> violation : violations) {
-                    sb.append("<li>").
-                            append(I18nMessages.INSTANCE.constraintViolation(
-                                    violation.getPropertyPath().toString(),
-                                    (String) violation.getInvalidValue(),
-                                    violation.getMessage()
-                            )).
-                            append("</li>");
-                }
-                sb.append("<ol>");
+        final StringBuilder sb = new StringBuilder();
+        sb.append("<ol>");
+        for (final ConstraintViolation<?> violation : violations) {
+          sb.append("<li>").
+              append(I18nMessages.INSTANCE.constraintViolation(
+                  violation.getPropertyPath().toString(),
+                  (String) violation.getInvalidValue(),
+                  violation.getMessage()
+              )).
+              append("</li>");
+        }
+        sb.append("<ol>");
 
-                new StatusBar(
-                        cf,
-                        SafeHtmlUtils.fromSafeConstant(sb.toString()),
-                        StatusBar.Kind.APP_ERROR,
-                        I18nConstants.INSTANCE.violations() + ": " + violations.size()
-                );
-            }
+        new StatusBar(
+            cf,
+            SafeHtmlUtils.fromSafeConstant(sb.toString()),
+            StatusBar.Kind.APP_ERROR,
+            I18nConstants.INSTANCE.violations() + ": " + violations.size()
+        );
+      }
 
-            @Override
-            public void onSuccess(Void response) {
-                dialog.hide();
-
-                cf.getEb().fireEvent(new RefreshEvent());
-                cf.getEb().fireEvent(new DoneEvent());
-                new StatusBar(
-                        cf,
-                        SafeHtmlUtils.fromSafeConstant(I18nConstants.INSTANCE.employeeUpdated()),
-                        StatusBar.Kind.SUCCESS,
-                        null);
-            }
-        });
-    }
-
-    @UiHandler("ok")
-    public void onOk(ClickEvent event) {
-        save();
-    }
-
-    @UiHandler("cancel")
-    public void onCancel(ClickEvent event) {
+      @Override
+      public void onSuccess(Void response) {
         dialog.hide();
-    }
 
-    private void fetchAndEdit() {
-        Request<EmployeeProxy> fetchRequest = (Request<EmployeeProxy>) cf.getRf().find(employee.stableId());
+        cf.getEb().fireEvent(new RefreshEvent());
+        cf.getEb().fireEvent(new DoneEvent());
+        new StatusBar(
+            cf,
+            SafeHtmlUtils.fromSafeConstant(I18nConstants.INSTANCE.employeeUpdated()),
+            StatusBar.Kind.SUCCESS,
+            null);
+      }
+    });
+  }
 
-        fetchRequest.with(Driver.DRIVER.getPaths());
+  @UiHandler("ok")
+  public void onOk(ClickEvent event) {
+    save();
+  }
 
-        fetchRequest.to(new Receiver<EntityProxy>() {
-            @Override
-            public void onSuccess(EntityProxy person) {
-                EmployeeReqCtx context = cf.getRf().employeeReqCtx();
-                edit(context);
-                context.put(employee);
-            }
-        }).fire();
-    }
+  @UiHandler("cancel")
+  public void onCancel(ClickEvent event) {
+    dialog.hide();
+  }
 
-    private void preventSibling() {
-        cf.getEb().fireEvent(new PreventSiblingEvent(this));
-        cf.getEb().addHandler(PreventSiblingEvent.TYPE, new PreventSiblingEvent.Handler() {
-            @Override
-            public void onSiblingExists(PreventSiblingEvent event) {
-                if (event.getKind() instanceof EmployeeEditorWorkflow) {
-                    dialog.hide();
-                }
-            }
-        });
-    }
+  private void fetchAndEdit() {
+    @SuppressWarnings("unchecked")
+    Request<EmployeeProxy> fetchRequest = cf.getRf().find((EntityProxyId<EmployeeProxy>) employee.stableId());
 
-    private void assignShortcuts() {
-        dialog.addDomHandler(new KeyUpHandler() {
-            public void onKeyUp(KeyUpEvent event) {
-                switch (event.getNativeKeyCode()) {
-                    case KeyCodes.KEY_ESCAPE:
-                        onCancel(null);
-                        break;
-                    case KeyCodes.KEY_ENTER:
-                        onOk(null);
-                        break;
-                }
-            }
-        }, KeyUpEvent.getType());
-    }
+    fetchRequest.with(Driver.DRIVER.getPaths());
 
-    EmployeeEditorWorkflow(ClientFactory cf, Boolean isNew) {
-        this.cf = cf;
-        Binder.BINDER.createAndBindUi(this);
-        assignShortcuts();
-        if (isNew) {
-            create();
+    fetchRequest.to(new Receiver<EmployeeProxy>() {
+      @Override
+      public void onSuccess(EmployeeProxy person) {
+        EmployeeReqCtx context = cf.getRf().employeeReqCtx();
+        edit(context);
+        context.put(employee);
+      }
+    }).fire();
+  }
+
+  private void preventSibling() {
+    cf.getEb().fireEvent(new PreventSiblingEvent(this));
+    cf.getEb().addHandler(PreventSiblingEvent.TYPE, new PreventSiblingEvent.Handler() {
+      @Override
+      public void onSiblingExists(PreventSiblingEvent event) {
+        if (event.getKind() instanceof EmployeeEditorWorkflow) {
+          dialog.hide();
         }
-    }
+      }
+    });
+  }
 
-    EmployeeEditorWorkflow(ClientFactory cf, EmployeeProxy employee) {
-        this(cf, false);
-        this.employee = employee;
-
-        edit(null);
-    }
-
-    private void show() {
-        dialog.center();
-        preventSibling();
-        employeeEditor.focus();
-        if (employee.getBirth() == null) {
-            employeeEditor.setDefaults();
+  private void assignShortcuts() {
+    dialog.addDomHandler(new KeyUpHandler() {
+      public void onKeyUp(KeyUpEvent event) {
+        switch (event.getNativeKeyCode()) {
+          case KeyCodes.KEY_ESCAPE:
+            onCancel(null);
+            break;
+          case KeyCodes.KEY_ENTER:
+            onOk(null);
+            break;
         }
+      }
+    }, KeyUpEvent.getType());
+  }
+
+  EmployeeEditorWorkflow(ClientFactory cf, Boolean isNew) {
+    this.cf = cf;
+    Binder.BINDER.createAndBindUi(this);
+    assignShortcuts();
+    if (isNew) {
+      create();
+    }
+  }
+
+  EmployeeEditorWorkflow(ClientFactory cf, EmployeeProxy employee) {
+    this(cf, false);
+    this.employee = employee;
+
+    edit(null);
+  }
+
+  private void show() {
+    dialog.center();
+    preventSibling();
+    employeeEditor.focus();
+    if (employee.getBirth() == null) {
+      employeeEditor.setDefaults();
+    }
+  }
+
+  void edit(EmployeeReqCtx reqCtx) {
+    Driver.DRIVER.initialize(cf.getRf(), employeeEditor);
+
+    if (reqCtx == null) {
+      fetchAndEdit();
+      return;
     }
 
-    void edit(EmployeeReqCtx reqCtx) {
-        Driver.DRIVER.initialize(cf.getRf(), employeeEditor);
+    Driver.DRIVER.edit(employee, reqCtx);
+    show();
 
-        if (reqCtx == null) {
-            fetchAndEdit();
-            return;
-        }
-
-        Driver.DRIVER.edit(employee, reqCtx);
-        show();
-
-        reqCtx.put(employee);
-    }
+    reqCtx.put(employee);
+  }
 }
