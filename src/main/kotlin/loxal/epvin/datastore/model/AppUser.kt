8a3ff8e504d624f7b8045cd77177d826d7c9c966
Alex <alexander.orlov@loxal.net>
2015-08-30 00:50:07 +0200

    make app more NPE-safe

--- src/main/kotlin/loxal/epvin/datastore/model/AppUser.kt
+++ src/main/kotlin/loxal/epvin/datastore/model/AppUser.kt
@@ -1,13 +1,16 @@
+/*
+ * Copyright 2015 Alexander Orlov <alexander.orlov@loxal.net>. All rights reserved.
+ */
+
 package loxal.epvin.datastore.model
 
 import com.googlecode.objectify.annotation.Entity
 
-
 Entity
 public class AppUser : DatastoreEntity() {
-    public var role: String? = null
+    public var role: String = ""
 
-    private val roleCtx: String? = null
+    private val roleCtx: String = ""
 
-    public var email: String? = null
+    public var email: String = ""
 }
