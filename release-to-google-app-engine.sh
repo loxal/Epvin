bc9c2ec3cd64f801b3589b746eec07bd4e38dcff
Alex <alexander.orlov@loxal.net>
2017-02-26 01:41:17 +0100

    actully deploy a working version of the app

--- release-to-google-app-engine.sh
+++ release-to-google-app-engine.sh
@@ -2,7 +2,4 @@
 
 # Set `<vm>false</vm>` in appengine-web.xml
 
-# Switch Java JDK
-#export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
-
-mvn clean compile gwt:compile appengine:deploy
+mvn clean compile gwt:compile appengine:update
